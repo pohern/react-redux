9709e5ed64eb176a149c270c116e2ffed3dfb925
pohern <ohern_p@yahoo.com>
2021-08-28 14:20:14 -0500

    add state and set default values

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-
-
+import React, { useState} from 'react';
 function App() {
+  // 1. Hardcode UI
+  // 2. Hardcode data, and set UI to use data
+  // 3. Dynamically get my data.
+  const [post, setPost] = useState({
+    userId: 1,
+    id: 1,
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  });
   return (
-    <div className="App">
+    <div className='container'>
+      <div className='row'>
+        <div className='col-6'>
+          <h2>{post.title}</h2>
+          <p>
+            <strong>User ID</strong> {post.userId}
+          </p>
+          <p>
+            <strong>Post ID</strong> {post.id}
+          </p>
+          <p>
+            {post.body}
+          </p>
+        </div>
+      </div>
       <h1>Hello World</h1>
     </div>
   );
